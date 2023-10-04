dd67c4c5c300fcfc03b744d39050ccc997c9c4dc
Treves Lindstrom <trevesmlindstrom@gmail.com>
2023-10-04 16:34:43 -0700

    first iteration

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sprint5project
+
+This project is taking US car advertisement data and lookingat aspects of the amount of the time the cars stay listed along with their price and make.
+
+The tool demonstrate a histogram looking at the days listed and a scatter plot looking at days listed vs price.
---
+++ app.py
@@ -0,0 +1,32 @@
+#import packages
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+#import data
+vehicles = pd.read_csv("C:\\Users\\treve\\sprint5project\\vehicles_us.csv")
+
+#split model into two new columns
+vehicles[['car_make', 'car_model']] = vehicles['model'].str.split(' ', n=1, expand=True)
+
+st.header('Days Listed from Car Advertisement')
+
+#histogram
+days_hist = px.histogram(filter_hist,x='days_listed',title='Days Listed Histogram')
+st.plotly_chart(days_hist)
+
+ #scatter
+
+checkbox_scatter = st.checkbox('Chart Toggle')
+
+if checkbox_scatter:
+    px.scatter(vehicles,x='price',y='days_listed',title='Price vs Days Listed Scatter',color='type')
+    st.plotly_chart(price_scatter)
+else: 
+    px.scatter(vehicles,x='price',y='days_listed',title='Price vs Days Listed Scatter',color='car_model')
+    st.plotly_chart(price_scatter)
+
+
+
+
+
