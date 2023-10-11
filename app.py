5575fb790bffc280af6f571c99f61565727675a7
Treves Lindstrom <trevesmlindstrom@gmail.com>
2023-10-11 14:04:45 -0700

    notebook fix and app checkbox change

--- app.py
+++ app.py
@@ -9,21 +9,34 @@ vehicles = pd.read_csv('vehicles_us.csv')
 #split model into two new columns
 vehicles[['car_make', 'car_model']] = vehicles['model'].str.split(' ', n=1, expand=True)
 
-st.header('Days Listed from Car Advertisement')
 
-#histogram
-days_hist = px.histogram(vehicles,x='days_listed',title='Days Listed Histogram')
-st.plotly_chart(days_hist)
+## create filtered data sets
 
- #scatter
+#top types filter
+vehicles['type'] = vehicles['type'].replace('pickup','truck')
+type_counts = vehicles['type'].value_counts().nlargest(5).index
+filtered_types = vehicles[vehicles["type"].isin(type_counts)]
 
-checkbox_scatter = st.checkbox('Scatter Chart Toggle')
+#top makes filter
+make_counts = vehicles['car_make'].value_counts().nlargest(5).index
+filtered_makes = vehicles[vehicles["car_make"].isin(make_counts)]
 
-if checkbox_scatter:
-    price_scatter = px.scatter(vehicles,x='price',y='days_listed',title='Price vs Days Listed Scatter',color='type')
+st.header('Days Listed from Car Advertisement for TOP 5 Car Makes and Types')
+
+checkbox_chart = st.checkbox('Chart Toggle')
+
+if checkbox_chart:
+#type charts
+    days_hist = px.histogram(filtered_types,x='days_listed',title='Days Listed Histogram',template='plotly',color='type').update_yaxes(title_text = 'frequency of listing length').update_xaxes(title_text = "days listed")
+    st.plotly_chart(days_hist)
+
+    price_scatter = px.scatter(filtered_types,x='price',y='days_listed',title='Price vs Days Listed Scatter',template='plotly',color='type').update_yaxes(title_text = "days listed")
     st.plotly_chart(price_scatter)
 else: 
-    price_scatter = px.scatter(vehicles,x='price',y='days_listed',title='Price vs Days Listed Scatter',color='car_make')
+    #make charts
+    days_hist = px.histogram(filtered_makes,x='days_listed',title='Days Listed Histogram',template='plotly',color='car_make').update_yaxes(title_text = 'frequency of listing length').update_xaxes(title_text = "days listed")
+    st.plotly_chart(days_hist)
+    price_scatter = px.scatter(filtered_makes,x='price',y='days_listed',title='Price vs Days Listed Scatter',template='plotly',color='car_make').update_yaxes(title_text = "days listed")
     st.plotly_chart(price_scatter)
 
 
